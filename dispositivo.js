5cb364c0f9e395e4c6c4bdf051a4b853f6ab2737
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2023-04-07 10:15:23 -0300

    Initial commit

---
+++ dispositivo.js
@@ -0,0 +1,59 @@
+/**
+ * Crição de um objeto chamado celular que tem como atributos o fabricante, modelo, situação e volume. 
+ * Possui ainda as funções de ligar, desligar, aumentar volume e baixar volume.
+ * ******************
+ * celular          *
+ * ******************
+ * fabricante       *
+ * modelo           *
+ * situacao         *
+ * ******************
+ * ligar()          *
+ * desligar()       *
+ * aumentarVolume() *
+ * baixarVolume()   *
+ * ****************** 
+ */
+
+var celular = {
+    fabricante: "Samsung",
+    modelo: "A31",
+    situacao: "off",
+    volume: 0,
+    
+    ligar() {
+        if (celular.situacao === "off") {
+            celular.situacao = "on";
+        }
+    },
+
+    aumentarVolume(){
+        celular.volume ++;
+    },
+
+    baixarVolume() {
+        celular.volume --;
+    },
+
+    desligar() {
+        if (celular.situacao === "on") {
+            celular.situacao = "off";
+        }
+    }    
+}
+
+console.log("Descrição :" + celular.fabricante + " - " + celular.modelo + " - " + celular.situacao + " - " + celular.volume);
+console.log("Situação inicial: " + celular.situacao);
+celular.ligar();
+console.log("Nova situação: " + celular.situacao);
+console.log("Volume inicial: " + celular.volume);
+celular.aumentarVolume();
+console.log("Novo volume: " + celular.volume);
+celular.aumentarVolume();
+console.log("Novo volume: " + celular.volume);
+celular.aumentarVolume();
+console.log("Novo volume: " + celular.volume);
+celular.baixarVolume();
+console.log("Novo volume: " + celular.volume);
+celular.desligar();
+console.log("Nova situação: " + celular.situacao);
